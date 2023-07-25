7b5be98d4455672df56daf82026002a480ee83e9
Goh Tuan Chuong <tuan.goh@student.manchester.ac.uk>
2023-07-25 20:28:33 +0100

    Increase size of ball

--- BouncingBallTutorial/BouncingBall.py
+++ BouncingBallTutorial/BouncingBall.py
@@ -6,6 +6,9 @@ screen = pew.Pix()
 x, y = 3, 0
 dx, dy = 1, 1
 
+ball = pew.Pix.from_iter(((3, 2),
+                          (2, 1)))
+
 background = pew.Pix.from_iter(((1, 0, 1, 0, 1, 0, 1, 0),
                                 (0, 1, 0, 1, 0, 1, 0, 1),
                                 (1, 0, 1, 0, 1, 0, 1, 0),
@@ -19,15 +22,15 @@ while True:
     keys = pew.keys()
     screen.blit(background)
 
-    if x + dx > 7 or x + dx < 0 or keys & pew.K_O:
+    if x + dx > 6 or x + dx < 0 or keys & pew.K_O:
         dx = -dx
-    if y + dy > 7 or y + dy < 0 or keys & pew.K_X:
+    if y + dy > 6 or y + dy < 0 or keys & pew.K_X:
         dy = -dy
 
     x += dx
     y += dy
 
-    screen.pixel(x,y,3)
+    screen.blit(ball, x, y)
 
     pew.show(screen)
     pew.tick(1/5)
