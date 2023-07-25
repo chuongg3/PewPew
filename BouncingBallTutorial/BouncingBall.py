8ede9b52a5eeeffc569788e58017001a16457b30
Goh Tuan Chuong <tuan.goh@student.manchester.ac.uk>
2023-07-25 19:41:53 +0100

    Bouncing Ball with button to change behaviour

--- BouncingBallTutorial/BouncingBall.py
+++ BouncingBallTutorial/BouncingBall.py
@@ -1,4 +1,5 @@
 import pew
+
 pew.init()
 screen = pew.Pix()
 
@@ -6,18 +7,18 @@ x, y = 3, 0
 dx, dy = 1, 1
 
 while True:
+    keys = pew.keys()
     screen.pixel(x,y,0)
 
-    
-
-    if x + dx > 7 or x + dx < 0:
+    if x + dx > 7 or x + dx < 0 or keys & pew.K_O:
         dx = -dx
-    if y + dy > 7 or y + dy < 0:
+    if y + dy > 7 or y + dy < 0 or keys & pew.K_X:
         dy = -dy
-        
+
     x += dx
     y += dy
 
     screen.pixel(x,y,3)
+
     pew.show(screen)
-    pew.tick(1/12)
+    pew.tick(1/5)
